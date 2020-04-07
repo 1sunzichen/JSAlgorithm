e089f0e8caf27d02a3477acb3678a2868f0ede0c
“sjq” <3023493319@qq.com>
2020-04-07 09:42:03 +0800

    fix lesson1

--- code/string/lesson1.js
+++ code/string/lesson1.js
@@ -1,14 +1,21 @@
-export default (str)=>{
+/* export default (str)=>{
   //  字符串按空格进行分隔 保存数组 数组的元素的先后顺序 就是 单词的顺序
-  let arr=str.split(' ');
-  console.log('====================================');
-  console.log(arr);
-  console.log('====================================');
+  let arr=str.split(/\s/g);
   // 对数组进行遍历 然后每个元素进行反转
   let result=arr.map(item=>{
-               字符串  边数组 颠倒  又变成 字符串 item
+         //      字符串  边数组 颠倒  又变成 字符串 item
     return item.split('').reverse().join('')
   })
-
   return result.join(' ')
-}
+} */
+
+ export default (str)=>{
+  //  字符串按空格进行分隔 保存数组 数组的元素的先后顺序 就是 单词的顺序
+ 
+  // 对数组进行遍历 然后每个元素进行反转
+  return str.match(/[\w']+/g).map(item=>{
+         //      字符串  边数组 颠倒  又变成 字符串 item
+    return item.split('').reverse().join('')
+  })
+  return result.join(' ')
+} 
